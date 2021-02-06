6b886369b888bb2d392ba0db1dea88553394fb18
Eric Yuan <me@yuane.ca>
2021-02-06 02:52:29 -0500

    Dummy commit

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ function App() {
 
   useEffect(() => {
     /* global fetch */
-    console.log(TOKEN)
     fetch(
       'https://raw.githubusercontent.com/ewyuan/sta313-design-critique/master/src/data/unemployment-data.geojson'
     )
